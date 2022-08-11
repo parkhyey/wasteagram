f9e41779132321b8b74eb3e6f67d7180f2d2ce1f
Rina Park <parkhyey@oregonstate.edu>
2022-08-10 20:34:29 -0700

    changed to named routes

--- lib/app.dart
+++ lib/app.dart
@@ -3,11 +3,11 @@ import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'screens/list_screen.dart';
+import 'screens/new_screen.dart';
 
 class App extends StatefulWidget {
-
   App({Key? key}) : super(key: key);
-  
+
   // EXTRA CREDIT: Sentry Crash Report
   static Future<void> reportError(dynamic error, dynamic stackTrace) async {
     final sentryId =
@@ -29,30 +29,41 @@ class App extends StatefulWidget {
 
   @override
   State<App> createState() => AppState();
+
 }
 
 class AppState extends State<App> {
+  // bool _initialized = false;
+  // bool _error = false;
 
-// @override
-// void initState() {
-//   App.observer;
-//   super.initState();
-// }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   initFirebase();
+  // }
+
+  // void initFirebase() async {
+  //   try {
+  //     await Firebase.initializeApp();
+  //     setState(() {
+  //       _initialized = true;
+  //     });
+  //   } catch(e) {
+  //     setState(() {
+  //       _error = true;
+  //     });
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // throw StateError('error demo');
-    // throw UnimplementedError('error demo');
     return MaterialApp(
-      title: 'Wasteagram',
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        brightness: Brightness.dark,
-      ),
-      // adding Firebase Analytics---------------------
-      // navigatorObservers: <NavigatorObserver>[App.observer],
-      // until here------------------------------------
-      home: ListScreen()
+        title: 'Wasteagram',
+        theme: ThemeData.dark(),
+        routes: {
+          '/': (context) => ListScreen(),
+          'new': (context) => NewScreen(),
+        }
     );
   }
-
 }
