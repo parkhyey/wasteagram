6d2cb07ca86f45465ac41731e61ff38229580178
Rina Park <parkhyey@oregonstate.edu>
2022-08-10 20:41:53 -0700

    clean up

--- lib/widgets/new_post_form.dart
+++ lib/widgets/new_post_form.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import '../models/food_waste_post_DTO.dart';
+import '../models/waste_post.dart';
 
- Widget newPostForm(BuildContext context, FoodWastePostDTO newPost){
+Widget newPostForm(BuildContext context, WastePost newPost){
 
-    return Semantics(
-      textField: true,
-      focusable: true,
-      child: TextFormField(
-        textAlign: TextAlign.center,
-        autofocus: true,
-        inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-        keyboardType: TextInputType.number,
-        decoration: const InputDecoration(
-          hintText: 'Number of Wasted Items',
-          contentPadding: EdgeInsets.zero,
-        ),
-        style: const TextStyle(fontSize: 25.0, height: 2.0),
-        onSaved: (value) {
-          newPost.quantity = int.tryParse(value!);
-        },
-        validator: (value) {
-          if (value!.isEmpty) {
-            return 'Please enter the number of items.';
-          }
-          return null;
-        },
+  return Semantics(
+    textField: true,
+    focusable: true,
+    child: TextFormField(
+      textAlign: TextAlign.center,
+      autofocus: true,
+      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+      keyboardType: TextInputType.number,
+      decoration: const InputDecoration(
+        hintText: 'Number of Wasted Items',
+        contentPadding: EdgeInsets.zero,
       ),
-    );
+      style: const TextStyle(fontSize: 25.0, height: 2.0),
+      onSaved: (value) {
+        newPost.quantity = int.tryParse(value!);
+      },
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter the number of items.';
+        }
+        return null;
+      },
+    ),
+  );
 
  }
