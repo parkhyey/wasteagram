85245c90c613f927c3cac147af4d552f328cfc84
Rina Park <parkhyey@oregonstate.edu>
2022-08-10 20:46:12 -0700

    clean up

--- lib/widgets/list_builder.dart
+++ lib/widgets/list_builder.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import '../models/waste_post.dart';
 import '../screens/detail_screen.dart';
 
 class ListBuilder extends StatefulWidget {
+  const ListBuilder({Key? key}) : super(key: key);
 
   @override
   State<ListBuilder> createState() => _ListBuilderState();
@@ -21,39 +23,32 @@ class _ListBuilderState extends State<ListBuilder> {
         .snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-          return const Center(child: CircularProgressIndicator());
-        } else {
-          return Column(
-            children: <Widget>[
-              Expanded(
-                child: ListView.builder(
-                  itemCount: snapshot.data!.docs.length,
-                  itemBuilder: (context, index) {
-                    var post = snapshot.data!.docs[index];
-                    DateTime time = post['date'].toDate();
+            return const Center(child: CircularProgressIndicator());
+        } 
+        return ListView.builder(
+          itemCount: snapshot.data!.docs.length,
+          itemBuilder: (context, index) {
+            var post = snapshot.data!.docs[index];
+            DateTime time = post['date'].toDate();
 
-                    return Semantics(
-                      label: 'Press for detail view.',
-                      button: true,
-                      enabled: true,
-                      child: ListTile(
-                        trailing: Text(post['quantity'].toString(),
-                          style: Theme.of(context).textTheme.headline6),
-                        title:  Text(DateFormat.yMMMMEEEEd().format(time),
-                          style: Theme.of(context).textTheme.headline6),
-                        onTap: () {
-                          Navigator.push(context, MaterialPageRoute(
-                              builder: (context) => DetailScreen(post: post)));
-                        },
-                      )
-                    );
-                  }
-                )
-              ), 
-            ],
-          );
-        }
-      }
+            return Semantics(
+              label: 'Press for detail view.',
+              button: true,
+              enabled: true,
+              child: ListTile(
+                trailing: Text(post['quantity'].toString(),
+                  style: Theme.of(context).textTheme.headline6),
+                title:  Text(DateFormat.yMMMMEEEEd().format(time),
+                  style: Theme.of(context).textTheme.headline6),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => DetailScreen(post: post)));
+                },
+              )
+            );
+          }
+        );
+      },
     );
   }
 
